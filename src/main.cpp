a59fc5a86aba8724bd5c8fde458e37e513f50915
Brian Pfretzschner <mail@brianp.de>
2023-11-17 20:26:51 +0100

    WLAN connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <ESP8266WiFi.h>
 
-void setup() {
+char ssid[] = "";
+char pass[] = "";
+
+void setup()
+{
+    Serial.begin(115200);
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, pass);
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(1000);
+        Serial.print("Waiting for WLAN connection...");
+    }
 }
 
-void loop() {
+void loop()
+{
+    Serial.print("Ready...");
+    delay(1000);
 }
